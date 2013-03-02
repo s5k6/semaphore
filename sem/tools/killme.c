8a5b878cd4675b2024765adbce707b06d70b5d4c
Stefan Klinger <git@stefan-klinger.de>
2013-03-02 13:52:25 +0000

    -

--- sem/tools/killme.c
+++ sem/tools/killme.c
@@ -1,38 +1,5 @@
 
-/* This tool is used only to test the signal forwarding capabilities of
-  `sem`.  `killme` tries to handle all signals, and prints out what it
-  catches.  You'll have to shoot it to death with -9.
-
-
-  Use this to test `killme`
-
-      ./sem foo -p -v3 -f -- ./killme
-
-      pid= pid of killme
-
-      kill -0 "$pid" && for i in $(seq 1 64); do
-        case "$i" in
-          9|19|32|33) echo "skipping $i";;
-          *) echo "sending $i"; kill -"$i" "$pid"; sleep 0.3; ;;
-        esac;
-      done;
-      kill -9 "$pid";
-
-    or
-    
-      pid= pid of sem
-
-      kill -0 "$pid" && for i in SIGHUP SIGINT SIGQUIT SIGTERM SIGUSR1 SIGUSR2 SIGCONT SIGTSTP SIGTTIN SIGTTOU; do
-         kill -"$i" "$pid"; sleep 0.3;
-      done;
-
-      
-
-  Use this to test `sem`
-
-      ./sem foo -i1 -t -x -v3 -- ./killme
-
-*/
+/* This tool prints a message for each signal it is able to catch. */
 
 // Feature Test Macro Requirements
 #define _POSIX_C_SOURCE 200809L
